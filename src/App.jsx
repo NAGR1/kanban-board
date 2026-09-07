706094e4845281ffe75e0d25cff68b1c5d9ebfb2
Nyein Chan <nyeinnaing743@gmail.com>
2026-09-07 22:25:58 +0700

    Restore application styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { HashRouter, Routes, Route } from 'react-router-dom'
-
+import './App.css'
 import Navbar from './components/Navbar'
 import KanbanBoard from './pages/KanbanBoard'
 import Dashboard from './pages/Dashboard'
