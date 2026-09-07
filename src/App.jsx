452420e0583d703b1d578e1285d7e2c17795d6df
Nyein Chan <nyeinnaing743@gmail.com>
2026-09-07 22:01:22 +0700

    Fix page refresh 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import KanbanBoard from './pages/KanbanBoard'
 import Dashboard from './pages/Dashboard'
